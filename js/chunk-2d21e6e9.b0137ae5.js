f4292ed39267e5294340735afc3e29fcf34800b5
Svetozar Iliev <svetozariliev13@gmail.com>
2021-01-18 22:19:48 +0200

    test

---
+++ js/chunk-2d21e6e9.b0137ae5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e6e9"],{d612:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{},[t("h1",[e._v("Каузи")])])}],a={name:"Kayzi"},u=a,i=t("2877"),s=Object(i["a"])(u,c,r,!1,null,"676d7488",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d21e6e9.b0137ae5.js.map
